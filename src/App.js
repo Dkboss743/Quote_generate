59de53f9608614f7757dab7c618c1ede78fdea5f
Dkboss743 <2019246@iiitdmj.ac.in>
2022-05-27 17:26:44 +0530

    hosted on firebase

--- src/App.js
+++ src/App.js
@@ -1,39 +1,48 @@
 import { Route, Routes, Link, Navigate } from "react-router-dom";
-
-import AddQuotes from "./components/pages/AddQuotes";
-import AllQuotes from "./components/pages/AllQuotes";
-import QuoteDetail from "./components/pages/QuoteDetail";
+import React, { Suspense } from "react";
 import Layout from "./components/layout/layout";
-import NotFound from "./components/pages/NotFound";
-import Comments from "./components/comments/Comments";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+const AddQuotes = React.lazy(() => import("./components/pages/AddQuotes"));
+const QuoteDetail = React.lazy(() => import("./components/pages/QuoteDetail"));
+const NotFound = React.lazy(() => import("./components/pages/NotFound"));
+const Comments = React.lazy(() => import("./components/comments/Comments"));
+const AllQuotes = React.lazy(() => import("./components/pages/AllQuotes"));
 function App() {
   return (
     <div>
       <Layout>
-        <Routes>
-          <Route
-            path="/"
-            element={<Navigate to="/all-quotes"></Navigate>}
-          ></Route>
-          <Route path="/add-quotes" element={<AddQuotes></AddQuotes>}></Route>
-          <Route path="/all-quotes" element={<AllQuotes></AllQuotes>}></Route>
-          <Route
-            path="/all-quotes/:quoteId/*"
-            element={<QuoteDetail></QuoteDetail>}
-          >
-            <Route path={"comments"} element={<Comments></Comments>}></Route>
+        <Suspense
+          fallback={
+            <div>
+              <LoadingSpinner></LoadingSpinner>
+            </div>
+          }
+        >
+          <Routes>
             <Route
-              path={""}
-              element={
-                <Link className="btn--flat centered" to={`comments`}>
-                  Load Comments
-                </Link>
-              }
+              path="/"
+              element={<Navigate to="/all-quotes"></Navigate>}
             ></Route>
-          </Route>
+            <Route path="/add-quotes" element={<AddQuotes></AddQuotes>}></Route>
+            <Route path="/all-quotes" element={<AllQuotes></AllQuotes>}></Route>
+            <Route
+              path="/all-quotes/:quoteId/*"
+              element={<QuoteDetail></QuoteDetail>}
+            >
+              <Route path={"comments"} element={<Comments></Comments>}></Route>
+              <Route
+                path={""}
+                element={
+                  <Link className="btn--flat centered" to={`comments`}>
+                    Load Comments
+                  </Link>
+                }
+              ></Route>
+            </Route>
 
-          <Route path="*" element={<NotFound></NotFound>}></Route>
-        </Routes>
+            <Route path="*" element={<NotFound></NotFound>}></Route>
+          </Routes>
+        </Suspense>
       </Layout>
     </div>
   );
